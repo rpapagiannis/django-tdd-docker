baaa80038341187a0675028b23024eaa8ec4177b
rpapagiannis <rpapagiannis@icloud.com>
2025-12-16 01:09:56 +0200

    Add POST endpoint for creating movies

---
+++ app/movies/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import MovieList
+
+urlpatterns = [
+    path("api/movies/", MovieList.as_view()),
+]
---
+++ app/tests/movies/test_views.py
@@ -0,0 +1,58 @@
+import json
+
+import pytest
+
+from movies.models import Movie
+
+@pytest.mark.django_db
+def test_add_movie(client):
+    movies = Movie.objects.all()
+    assert len(movies) == 0
+
+    resp = client.post(
+        "/api/movies/",
+        {
+            "title": "The Big Lebowski",
+            "genre": "Comedy",
+            "year": "1998",
+        },
+        content_type="application/json"
+    )
+    assert resp.status_code == 201
+    assert resp.data["title"] == "The Big Lebowski"
+
+    movies = Movie.objects.all()
+    assert len(movies) == 1 
+
+@pytest.mark.django_db
+def test_add_movie_invalid_json(client):
+    movies = Movie.objects.all()
+    assert len(movies) == 0
+
+    resp = client.post(
+        "/api/movies/",
+        {},
+        content_type="application/json"
+    )
+    assert resp.status_code == 400
+
+    movies = Movie.objects.all()
+    assert len(movies) == 0
+
+@pytest.mark.django_db
+def test_add_movie_invalid_json_keys(client):
+    movies = Movie.objects.all()
+    assert len(movies) == 0
+
+    resp = client.post(
+        "/api/movies/",
+        {
+            "title": "The Big Lebowski",
+            "genre": "Comedy",
+        },
+        content_type="application/json"
+    )
+    assert resp.status_code == 400
+
+    movies = Movie.objects.all()
+    assert len(movies) == 0
