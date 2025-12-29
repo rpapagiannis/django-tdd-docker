465aaba3b8703002918a1030339e700a2fbc0e64
rpapagiannis <rpapagiannis@icloud.com>
2025-12-29 16:58:19 +0200

    Add WhiteNoise for static file serving in production

--- app/drf_project/settings.py
+++ app/drf_project/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -142,3 +143,7 @@ if not DEBUG:
             "rest_framework.renderers.JSONRenderer",
         )
     }
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
